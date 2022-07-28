ba0612bc6dccb91dc19d8a073453020cb326633f
hanyas <hanyas@users.noreply.github.com>
2022-07-28 20:33:13 +0300

    two mpc examples with jitted and unjitted ilqr

---
+++ examples/ilqr/pendulum_mpc_jax.py
@@ -0,0 +1,246 @@
+from typing import Callable
+from functools import partial
+
+from jax.config import config
+config.update("jax_enable_x64", True)
+# config.update("jax_log_compiles", 1)
+
+import jax.numpy as jnp
+import jax.random as jr
+
+from jax import jit
+from jax import block_until_ready
+from jax.lax import fori_loop, scan
+
+from tox.spaces import Box
+from tox.objects import Trajectory
+from tox.utils import runge_kutta
+from tox.solvers import ilqr
+from tox.solvers.ilqr import Hyperparameters, LinearPolicy
+
+import time as clock
+import matplotlib.pyplot as plt
+
+
+simulation_step = 0.05
+downsampling = 1
+
+state_dim = 2
+action_dim = 1
+
+
+def wrap_angle(x):
+    # wrap angle between [0, 2*pi]
+    return x % (2.0 * jnp.pi)
+
+
+def final_cost(state: jnp.ndarray) -> float:
+    goal: jnp.ndarray = jnp.array([jnp.pi, 0.0])
+    final_state_cost: jnp.ndarray = jnp.diag(jnp.array([1e0, 1e-1]))
+
+    _wrapped = jnp.hstack((wrap_angle(state[0]), state[1]))
+    c = (_wrapped - goal).T @ final_state_cost @ (_wrapped - goal)
+    return c
+
+
+def transient_cost(
+    state: jnp.ndarray, action: jnp.ndarray, time: int
+) -> float:
+
+    goal: jnp.ndarray = jnp.array([jnp.pi, 0.0])
+    state_cost: jnp.ndarray = jnp.diag(jnp.array([1e0, 1e-1]))
+    action_cost: jnp.ndarray = jnp.diag(jnp.array([1e-3]))
+
+    _wrapped = jnp.hstack((wrap_angle(state[0]), state[1]))
+    c = (_wrapped - goal).T @ state_cost @ (_wrapped - goal)
+    c += action.T @ action_cost @ action
+    return c
+
+
+def pendulum(
+    state: jnp.ndarray, action: jnp.ndarray, time: int
+) -> jnp.ndarray:
+
+    gravity: float = 9.81
+    length: float = 1.0
+    mass: float = 1.0
+    damping: float = 1e-3
+
+    position, velocity = state
+    return jnp.hstack(
+        (
+            velocity,
+            -3.0 * gravity / (2.0 * length) * jnp.sin(position)
+            + 3.0 * (action - damping * velocity) / (mass * length**2),
+        )
+    )
+
+
+state_space: Box = Box(
+    low=jnp.ones((state_dim,)) * jnp.finfo(jnp.float64).min,
+    high=jnp.ones((state_dim,)) * jnp.finfo(jnp.float64).max,
+    shape=(state_dim,),
+)
+
+action_space: Box = Box(
+    low=-2.5 * jnp.ones((action_dim,)),
+    high=2.5 * jnp.ones((action_dim,)),
+    shape=(action_dim,),
+)
+
+
+def dynamics(
+    state: jnp.ndarray,
+    action: jnp.ndarray,
+    time: int,
+) -> jnp.ndarray:
+    def _step(t, state):
+        next_state = runge_kutta(
+            state,
+            action,
+            time + t * simulation_step,
+            pendulum,
+            simulation_step,
+        )
+        return next_state
+
+    return fori_loop(
+        lower=0,
+        upper=downsampling,
+        body_fun=_step,
+        init_val=state,
+    )
+
+
+@partial(jit, static_argnums=(0, 1, 2, -1))
+def mpc_rollout(
+    final_cost: Callable,
+    transient_cost: Callable,
+    dynamics: Callable,
+    state_space: Box,
+    policy: LinearPolicy,
+    action_space: Box,
+    reference: Trajectory,
+    init_state: jnp.ndarray,
+    options: Hyperparameters,
+    nb_steps: int,
+) -> (jnp.ndarray, jnp.ndarray):
+    def mpc_step(carry, args):
+        policy, reference, state = carry
+
+        next_policy, next_reference = ilqr.jax_solver(
+            final_cost,
+            transient_cost,
+            dynamics,
+            state_space,
+            policy,
+            action_space,
+            reference,
+            state,
+            options,
+        )
+
+        action = next_reference.action[0]
+        next_state = state_space.clip(dynamics(state, action, 0))
+
+        return (next_policy, next_reference, next_state), (next_state, action)
+
+    _, (state, action) = scan(
+        mpc_step, init=(policy, reference, init_state), xs=jnp.arange(nb_steps)
+    )
+
+    state = jnp.vstack((init_state, state))
+    return state, action
+
+
+key = jr.PRNGKey(747)
+key, policy_key = jr.split(key, 2)
+
+horizon = 25
+reference = Trajectory(
+    state=jnp.zeros((horizon + 1, state_dim)),
+    action=jnp.zeros((horizon, action_dim)),
+)
+
+policy = ilqr.LinearPolicy(
+    K=jnp.zeros((horizon, action_dim, state_dim)),
+    kff=1e-2 * jr.normal(policy_key, shape=(horizon, action_dim)),
+)
+
+init_state = jnp.array([0.01, -0.01])
+options = ilqr.Hyperparameters(max_iter=25)
+
+nb_steps = 100
+
+start = clock.time()
+state, action = mpc_rollout(
+    final_cost,
+    transient_cost,
+    dynamics,
+    state_space,
+    policy,
+    action_space,
+    reference,
+    init_state,
+    options,
+    nb_steps,
+)
+block_until_ready(state)
+end = clock.time()
+print("Compilation + Execution Time:", end - start)
+
+plt.subplot(3, 1, 1)
+plt.plot(state[:, 0].T)
+plt.ylabel("x1")
+plt.subplot(3, 1, 2)
+plt.plot(state[:, 1].T)
+plt.ylabel("x2")
+plt.subplot(3, 1, 3)
+plt.plot(action[:, 1].T)
+plt.ylabel("u")
+plt.xlabel("t")
+plt.show()
+
+
+key, policy_key = jr.split(key, 2)
+
+reference = Trajectory(
+    state=jnp.zeros((horizon + 1, state_dim)),
+    action=jnp.zeros((horizon, action_dim)),
+)
+
+policy = ilqr.LinearPolicy(
+    K=jnp.zeros((horizon, action_dim, state_dim)),
+    kff=1e-2 * jr.normal(policy_key, shape=(horizon, action_dim)),
+)
+
+init_state = jnp.array([-0.01, 0.01])
+
+start = clock.time()
+state, action = mpc_rollout(
+    final_cost,
+    transient_cost,
+    dynamics,
+    state_space,
+    policy,
+    action_space,
+    reference,
+    init_state,
+    options,
+    nb_steps,
+)
+block_until_ready(state)
+end = clock.time()
+print("Execution Time:", end - start)
+
+plt.subplot(3, 1, 1)
+plt.plot(state[:, 0].T)
+plt.ylabel("x1")
+plt.subplot(3, 1, 2)
+plt.plot(state[:, 1].T)
+plt.ylabel("x2")
+plt.subplot(3, 1, 3)
+plt.plot(action[:, 1].T)
+plt.ylabel("u")
+plt.xlabel("t")
+plt.show()
---
+++ examples/ilqr/pendulum_mpc_py.py
@@ -0,0 +1,168 @@
+from jax.config import config
+config.update("jax_enable_x64", True)
+# config.update("jax_log_compiles", 1)
+
+import jax.numpy as jnp
+import jax.random as jr
+
+from jax.lax import fori_loop
+
+from tox.spaces import Box
+from tox.objects import Trajectory
+from tox.utils import runge_kutta
+from tox.solvers import ilqr
+
+import time as clock
+import matplotlib.pyplot as plt
+
+
+simulation_step = 0.01
+downsampling = 5
+
+state_dim = 2
+action_dim = 1
+
+
+def wrap_angle(x):
+    # wrap angle between [0, 2*pi]
+    return x % (2.0 * jnp.pi)
+
+
+def final_cost(state: jnp.ndarray) -> float:
+    goal: jnp.ndarray = jnp.array([jnp.pi, 0.0])
+    final_state_cost: jnp.ndarray = jnp.diag(jnp.array([1e0, 1e-1]))
+
+    _wrapped = jnp.hstack((wrap_angle(state[0]), state[1]))
+    c = (_wrapped - goal).T @ final_state_cost @ (_wrapped - goal)
+    return c
+
+
+def transient_cost(
+    state: jnp.ndarray, action: jnp.ndarray, time: int
+) -> float:
+
+    goal: jnp.ndarray = jnp.array([jnp.pi, 0.0])
+    state_cost: jnp.ndarray = jnp.diag(jnp.array([1e0, 1e-1]))
+    action_cost: jnp.ndarray = jnp.diag(jnp.array([1e-3]))
+
+    _wrapped = jnp.hstack((wrap_angle(state[0]), state[1]))
+    c = (_wrapped - goal).T @ state_cost @ (_wrapped - goal)
+    c += action.T @ action_cost @ action
+    return c
+
+
+def pendulum(
+    state: jnp.ndarray, action: jnp.ndarray, time: int
+) -> jnp.ndarray:
+
+    gravity: float = 9.81
+    length: float = 1.0
+    mass: float = 1.0
+    damping: float = 1e-3
+
+    position, velocity = state
+    return jnp.hstack(
+        (
+            velocity,
+            -3.0 * gravity / (2.0 * length) * jnp.sin(position)
+            + 3.0 * (action - damping * velocity) / (mass * length**2),
+        )
+    )
+
+
+state_space: Box = Box(
+    low=jnp.ones((state_dim,)) * jnp.finfo(jnp.float64).min,
+    high=jnp.ones((state_dim,)) * jnp.finfo(jnp.float64).max,
+    shape=(state_dim,),
+)
+
+action_space: Box = Box(
+    low=-2.5 * jnp.ones((action_dim,)),
+    high=2.5 * jnp.ones((action_dim,)),
+    shape=(action_dim,),
+)
+
+
+def dynamics(
+    state: jnp.ndarray,
+    action: jnp.ndarray,
+    time: int,
+) -> jnp.ndarray:
+    def _step(t, state):
+        next_state = runge_kutta(
+            state,
+            action,
+            time + t * simulation_step,
+            pendulum,
+            simulation_step,
+        )
+        return next_state
+
+    return fori_loop(
+        lower=0,
+        upper=downsampling,
+        body_fun=_step,
+        init_val=state,
+    )
+
+
+nb_steps = 100
+horizon = 25
+
+key = jr.PRNGKey(1337)
+
+state = jnp.zeros((nb_steps + 1, state_dim))
+action = jnp.zeros((nb_steps, action_dim))
+
+init_state = jnp.array([0.01, 0.0])
+state = state.at[0].set(init_state)
+
+reference = Trajectory(
+    state=jnp.zeros((horizon + 1, state_dim)),
+    action=jnp.zeros((horizon, action_dim)),
+)
+
+key, policy_key = jr.split(key, 2)
+policy = ilqr.LinearPolicy(
+    K=jnp.zeros((horizon, action_dim, state_dim)),
+    kff=1e-2 * jr.normal(policy_key, shape=(horizon, action_dim)),
+)
+
+options = ilqr.Hyperparameters(max_iter=25)
+
+start = clock.time()
+for t in range(nb_steps):
+
+    policy, reference, trace = ilqr.py_solver(
+        final_cost,
+        transient_cost,
+        dynamics,
+        state_space,
+        policy,
+        action_space,
+        reference,
+        state[t],
+        options,
+    )
+
+    action = action.at[t].set(reference.action[0])
+    state = state.at[t + 1].set(
+        state_space.clip(dynamics(state[t], action[t], t))
+    )
+
+    print("Time Step:", t, "Cost:", trace[-1])
+
+end = clock.time()
+print("Compilation + Execution Time:", end - start)
+
+plt.subplot(3, 1, 1)
+plt.plot(state[:, 0].T)
+plt.ylabel("x1")
+plt.subplot(3, 1, 2)
+plt.plot(state[:, 1].T)
+plt.ylabel("x2")
+plt.subplot(3, 1, 3)
+plt.plot(action[:, 1].T)
+plt.ylabel("u")
+plt.xlabel("t")
+plt.show()
