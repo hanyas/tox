ba0b1d9b2880a9916000efee0c53f2d557a216a9
hanyas <hanyas@users.noreply.github.com>
2022-08-03 21:54:53 +0300

    add generic discretization function

--- tox/utils.py
+++ tox/utils.py
@@ -1,6 +1,12 @@
 from typing import Callable
 
 import jax.numpy as jnp
+from jax.lax import fori_loop
+
+
+def wrap_angle(x: float) -> float:
+    # wrap angle between [0, 2*pi]
+    return x % (2.0 * jnp.pi)
 
 
 def symmetrize(x: jnp.ndarray) -> jnp.ndarray:
@@ -19,3 +25,30 @@ def runge_kutta(
     k3 = ode(state + 0.5 * step * k2, action, time)
     k4 = ode(state + step * k3, action, time)
     return state + step / 6.0 * (k1 + 2.0 * k2 + 2.0 * k3 + k4)
+
+
+def discretize_dynamics(ode: Callable, simulation_step: float, downsampling: int):
+    def dynamics(
+        state: jnp.ndarray,
+        action: jnp.ndarray,
+        time: int,
+    ):
+
+        def _step(t, state):
+            next_state = runge_kutta(
+                state,
+                action,
+                time + t * simulation_step,
+                ode,
+                simulation_step,
+            )
+            return next_state
+
+        return fori_loop(
+            lower=0,
+            upper=downsampling,
+            body_fun=_step,
+            init_val=state,
+        )
+
+    return dynamics
